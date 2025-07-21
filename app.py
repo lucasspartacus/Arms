25ea9292bef13ee10af17de40437ddd5e3f85124
Lucas Spartacus <50811095+lucasspartacus@users.noreply.github.com>
2025-07-21 10:00:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,7 @@ def processar():
 
     return render_template("resultado.html", resultados=resultados, shortlist=shortlist)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
